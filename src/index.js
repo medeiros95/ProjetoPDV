7afa58f973d12594ff8a0e51e7838ec4c3e756a4
victor allisson <victor.allisson95@gmail.com>
2023-11-28 22:28:41 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+const cors = require('cors')
 const express = require('express');
 const rotas = require('./rotas.js');
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use(rotas);
 
