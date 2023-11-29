46d689874439a952777f67ced1648f92e788a93e
victor allisson <victor.allisson95@gmail.com>
2023-11-28 23:06:29 -0300

    1

--- src/controladores/listarCategorias.js
+++ src/controladores/listarCategorias.js
@@ -4,7 +4,7 @@ const listarCategorias = async (req, res) => {
     try {
         const categorias = await knex('categorias').pluck('descricao')
 
-        res.status(200).json(categorias)
+        return  res.status(200).json(categorias)
     } catch (error) {
         return res.status(500).json({
             mensagem: 'Erro interno do servidor'
